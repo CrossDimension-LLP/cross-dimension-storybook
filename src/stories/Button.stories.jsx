eb4e50222b45c405b4151a7709bda542464be56b
Saumya999 <saumyadiptasarkar49@gmail.com>
2022-12-04 13:24:45 +0530

    Added changes for template

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,23 +1,36 @@
-// import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import React from 'react';
 
-import Button from '../components/Button/button';
+import { Button } from './Button';
 
-storiesOf('Button', module)
-  .add('with primary', () => <Button label="Primary Button" />)
-  .add('with outline', () => <Button
-    onClick={action('click')}
-    label="Ouline Button"
-    style={{ background: 'transparent', border: '3px solid #fecd43' }}
-  />)
-  .add('with rounder corners', () => <Button
-    label="Rounded Button"
-    onClick={action('click')}
-    style={{ borderRadius: '15px' }}
-  />)
-  .add('disabled', () => <Button disabled
-    label="Disabled Button"
-    onClick={action('click')}
-    style={{ background: 'gray' , border: 'gray', cursor: 'not-allowed' }}
-  />);
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+const Template = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
